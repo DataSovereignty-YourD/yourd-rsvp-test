fb1e66aa969b5f9cad535dc48165ffbd65e202a9
yeomjaeseung <struckpang@gmail.com>
2023-10-13 20:45:15 +0900

    apple

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,3 +1,7 @@
 export default function LoginPage() {
-  return <div>loginPage</div>;
+  return (
+    <div className="">
+      <div className="w-full h-1/2 bg-black">로그인</div>
+    </div>
+  );
 }
